df82c6ec8d88965919478d2495f25455f17b8ec5
maor <csmaor@gmail.com>
2018-02-17 14:09:12 +0200

    add controller one & two with rest calls

---
+++ dockertest/controllerone/src/main/java/Runner.java
@@ -0,0 +1,19 @@
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@EnableAutoConfiguration
+public class Runner {
+
+    @RequestMapping("/rest")
+    String home() {
+        return "Hello From Controller One!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Runner.class, args);
+    }
+}
